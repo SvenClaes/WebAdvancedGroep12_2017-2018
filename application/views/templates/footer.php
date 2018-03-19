5564eabb4aebb1a28b7241358636aad27ece6824
tibeauschodts <tibeau.schodts@student.pxl.be>
2018-03-19 17:09:24 +0100

    Basis voor opdracht 1
    Dit is zo goed als af... Als ge ergens aan bezig bent moet het hoogstwaarschijnlijk in de branch van opdracht 2.

---
+++ application/views/templates/footer.php
@@ -0,0 +1,8 @@
+<footer class="py-5 bg-dark">
+    <div class="container">
+        <p class="m-0 text-center text-white">Met liefde gemaakt door Tibeau, Thomas, Sven en Joran</p>
+    </div>
+</footer>
+
+</body>
+</html>
---
+++ application/views/templates/header.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Web Advanced</title>
+
+    <meta charset="utf-8">
+    <meta name="description" content="Web Advanced PE">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"/>
+    <style>
+        header {
+            padding: 154px 0 100px;
+            background-color: #ee4323;
+        }
+
+        @media (min-width: 992px) {
+            header {
+                padding: 156px 0 100px;
+            }
+        }
+
+        section {
+            padding: 150px 0;
+        }
+
+        section.team img {
+            width: 100px;
+        }
+    </style>
+</head>
+<body>
+
+<!-- Navigation -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+    <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="#page-top">Web Advanced</a>
+        <div id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link js-scroll-trigger" href="">Logboek</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link js-scroll-trigger" href="">Locaties</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link js-scroll-trigger" href="">Admin</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
