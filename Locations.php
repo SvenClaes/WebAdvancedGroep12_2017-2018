cffeca8b161197f0a4f89097519f3fd9aca2fb10
SvenClaes <sven.claes@student.pxl.be>
2018-04-13 22:56:53 +0200

    Scripts stories B
    Locations is het script dat ge in uw model plaatst voor aanmaken, verwijderen, aanpassen van locaties voor de admin en locaties is uw pagina me daarin de select data. Ik denk dat deze in een aparte php file moet en ook in model moet geplaatst worden.

---
+++ Locations.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 11500391
+ * Date: 13/04/2018
+ * Time: 22:06
+ */
+
+
+
+class Locations
+{
+
+
+
+
+    public function MakeLocation($name){
+        $servername = "vagrant";
+        $username = "root";
+        $password = "root";
+        $dbname = "groepswerk";
+
+        try {
+            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+            // set the PDO error mode to exception
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $sql = "INSERT INTO MyGuests (name)
+            VALUES ($name)";
+            // use exec() because no results are returned
+            $conn->exec($sql);
+            echo "New record created successfully";
+        }
+        catch(PDOException $e)
+        {
+            echo $sql . "<br>" . $e->getMessage();
+        }
+
+        $conn = null;
+    }
+
+    public function ChangeLocation($name,$id){
+
+        $servername = "vagrant";
+        $username = "root";
+        $password = "root";
+        $dbname = "groepswerk";
+
+        try {
+            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+            // set the PDO error mode to exception
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            $sql = "UPDATE locaties SET name=$name WHERE id=$id";
+
+            // Prepare statement
+            $stmt = $conn->prepare($sql);
+
+            // execute the query
+            $stmt->execute();
+
+            // echo a message to say the UPDATE succeeded
+            echo $stmt->rowCount() . " records UPDATED successfully";
+        }
+        catch(PDOException $e)
+        {
+            echo $sql . "<br>" . $e->getMessage();
+        }
+
+        $conn = null;
+
+    }
+
+    public function DeleteLocation($name){
+
+        $servername = "vagrant";
+        $username = "root";
+        $password = "root";
+        $dbname = "groepswerk";
+
+        try {
+            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+            // set the PDO error mode to exception
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            // sql to delete a record
+            $sql = "DELETE FROM locaties WHERE name= $name ";
+
+            // use exec() because no results are returned
+            $conn->exec($sql);
+            echo "Record deleted successfully";
+        }
+        catch(PDOException $e)
+        {
+            echo $sql . "<br>" . $e->getMessage();
+        }
+
+        $conn = null;
+    }
+
+}
+
