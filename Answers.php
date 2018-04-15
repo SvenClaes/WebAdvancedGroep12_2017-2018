948f6cbb03347da052a755bb12291f8871a916dc
jorandirkzwager <11501843@student.pxl.be>
2018-04-15 12:58:39 +0200

    Create Answers.php

---
+++ Answers.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: joran
+ * Date: 15/04/2018
+ * Time: 12:41
+ */
+class Answers
+{
+   public function DeleteAnswersPerLocation($location)
+   {
+        $servername = "vagrant";
+        $username = "root";
+        $password = "root";
+        $dbname = "groepswerk";
+        try {
+            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+            // set the PDO error mode to exception
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            // sql to do delete all answers from a given location
+            $sql = "DELETE FROM answers WHERE location = $location";
+            // use exec() because no results are returned
+            $conn->exec($sql);
+            echo "Record deleted successfully";
+        }
+        catch(PDOException $e)
+        {
+            echo $sql . "<br>" . $e->getMessage();
+        }
+        $conn = null;
+    }
+ }
