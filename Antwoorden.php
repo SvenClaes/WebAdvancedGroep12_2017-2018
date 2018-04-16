b3e337cc4879defa7bcc7a8a89a87117c4798884
jorandirkzwager <11501843@student.pxl.be>
2018-04-16 11:25:18 +0200

    Update and rename Answers.php to Antwoorden.php

--- Antwoorden.php
+++ Antwoorden.php
@@ -5,9 +5,9 @@
  * Date: 15/04/2018
  * Time: 12:41
  */
-class Answers
+class Antwoorden
 {
-   public function DeleteAnswersPerLocation($location)
+   public function DeleteAnswersPerLocation($locatieId)
    {
         $servername = "vagrant";
         $username = "root";
@@ -18,7 +18,7 @@ class Answers
             // set the PDO error mode to exception
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             // sql to do delete all answers from a given location
-            $sql = "DELETE FROM answers WHERE location = $location";
+            $sql = "DELETE FROM antwoorden WHERE locatieId = $locatieId";
             // use exec() because no results are returned
             $conn->exec($sql);
             echo "Record deleted successfully";
